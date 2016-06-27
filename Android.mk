5a9d69879ccde59326e9d45fd596512c1bb916df
bryan2894 <bryan2894@aospal.com>
2016-06-27 14:05:03 -0400

    Undo pathmap
    Change-Id: I7dc66261c273c320f695331ae903cf7a4ab848e2

--- Android.mk
+++ Android.mk
@@ -1,9 +1,5 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media))
-
-ifneq ($(filter msm8960 msm8660,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
 
 include $(call all-subdir-makefiles)
-
-endif
 
 endif
